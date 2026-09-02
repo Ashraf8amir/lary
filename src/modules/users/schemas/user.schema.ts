b0fc50946eaf4b3ec0e6045bbf224729549c6002
Ashraf Samir <achrafsamir808@gmail.com>
2026-09-02 20:40:06 +0300

    feat: create user mongoose schema and interfaces

---
+++ src/modules/users/schemas/user.schema.ts
@@ -0,0 +1,42 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { UserStatus } from '../enums/user-status.enum';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({
+  timestamps: true,
+  collection: 'users',
+  versionKey: false,
+})
+export class User {
+  @Prop({
+    required: true,
+    lowercase: true,
+    trim: true,
+    maxLength: 254,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Invalid email format'],
+  })
+  email!: string;
+
+  @Prop({
+    type: String,
+    required: false,
+    default: '',
+    trim: true,
+    maxLength: 100,
+  })
+  fullName!: string;
+
+  @Prop({
+    type: String,
+    enum: Object.values(UserStatus),
+    default: UserStatus.Active,
+    index: true,
+  })
+  status!: UserStatus;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.index({ email: 1 }, { unique: true });
